1ad503a8ad3082b2e2ff5bedae9496a750df762a
Nuah <nuah@Mac-mini.local>
2022-10-23 16:53:39 -0600

    Added Contact Page Functionality

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,20 +1,56 @@
+import { useState } from 'react';
 import '../css/Contact.css';
 
 const Contact = () => {
+    const [form, setForm] = useState({name: '', email: '', message: ''});
+
+    function onChangeName (e) {
+        setForm({...form, name: e.currentTarget.value});
+    }
+
+    function onChangeEmail (e) {
+        setForm({...form, email: e.currentTarget.value});
+    }
+
+    function onChangeMessage (e) {
+        setForm({...form, message: e.currentTarget.value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch('/send', {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {
+                "Accept": 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }).then(
+            (response) => (response.json())
+        ).then((response) => {
+            if (response.status === 'success') {
+                alert('Message Sent');
+                setForm({name: '', email: '', message: ''});
+            } else if (response.status === 'fail') {
+                alert('Message failed to send');
+            }
+        })
+    }
+
     return (
         <div className='mainPage contactPage'>
             <h1>Contact</h1>
-            <form>
+            <form onSubmit={(e) => {handleSubmit(e)}}>
                 <p>&emsp;If you would like to send me a message, here is the spot to do it. Fill out your message and hit submit, and I'll do my best to get back to you as soon as possible.</p>
 
                 <label>Name:</label>
-                <input type='text' />
+                <input type='text' value={form.name} onChange={(e) => {onChangeName(e)}}/>
 
                 <label>Email:</label>
-                <input type='email' />
+                <input type='email' value={form.email} onChange={(e) => {onChangeEmail(e)}}/>
 
                 <label>Message:</label>
-                <textarea type='text' />
+                <textarea type='text' value={form.message} onChange={(e) => {onChangeMessage(e)}}/>
 
                 <div className='buttonContainer'><button type='submit'>Submit</button></div>
             </form>
--- routes/record.js
+++ routes/record.js
@@ -1,10 +1,47 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 
 const recordRoutes = express.Router();
 
 const dbo = require('../conn');
 
 const ObjectId = require('mongodb').ObjectId;
+
+const transport = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APPKEY
+    }
+}
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+    if (error) {console.log(error)}
+    else {console.log('Server is ready to take messages.')}
+});
+
+recordRoutes.route("/send").post((req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+    const content = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+    const mail = {
+        from: name,
+        to: 'noah@noahselectionmachine.com',
+        subject: 'New Contact Form Message',
+        text: content
+    }
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({status: 'fail'});
+        } else {
+            res.json({status: 'success'});
+        }
+    })
+})
 
 recordRoutes.route("/resultsRecord").get(function (req, res) {
     let [candidatesDB, resultsDB] = dbo.getDB(); 
